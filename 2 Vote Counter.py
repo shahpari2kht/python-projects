f673a37bd0f24f2e1779c982de5514ae78afa6b5
Parisa Mohammadzadeh <110891110+shahpari2kht@users.noreply.github.com>
2025-05-23 13:38:20 +0330

    Empty  Vote Counter.py  file before deletion (no delete option in UI)

--- 2 Vote Counter.py
+++ 2 Vote Counter.py
@@ -1,17 +1,1 @@
-n=int(input())
 
-vote={}
-vote1=dict()
-
-for i in range(n):
-	s=input()	
-	if s in list(vote.keys()):
-		vote[s]+=1
-	else:
-		vote[s]=1
-
-z=list(vote.keys())
-z.sort()
-for i in z:
-	print(i,vote[i])
- 
